f8db84e57183ae6d812062367a50be1bde6fd465
Oleksandr Vedmedenko <stbearua@gmail.com>
2023-06-30 22:08:20 +0200

    resize +

--- controllers/user/updateAvatar.js
+++ controllers/user/updateAvatar.js
@@ -1,28 +1,27 @@
 const path = require("path");
 const fs = require("fs/promises");
-const Jimp = require("jimp");
 const { User } = require("../../models/user");
+const { resize } = require("../../helpers");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
+
   const { path: tempUpload, originalname } = req.file;
 
-  Jimp.read(tempUpload)
-    .then((image) => {
-      return image.resize(250, 250); // resize
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  await resize(tempUpload);
 
   const filename = `${_id}_${originalname}`;
 
   const resultUpload = path.join(avatarsDir, filename);
+
   await fs.rename(tempUpload, resultUpload);
+
   const avatarURL = path.join("avatars", filename);
+
   await User.findByIdAndUpdate(_id, { avatarURL });
+
   res.json({ avatarURL });
 };
 
--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,6 @@
 const HttpError = require("./HttpError");
 const ctrlWrapper = require("./ctrlWrapper");
 const handleMongooseError = require("./handleMongooseError");
+const resize = require("./resize");
 
-module.exports = { HttpError, ctrlWrapper, handleMongooseError };
+module.exports = { HttpError, ctrlWrapper, handleMongooseError, resize };
